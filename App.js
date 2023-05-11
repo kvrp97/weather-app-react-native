97e3cc8bf6ad2b959fe03c95b010522237df4f4a
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-05-11 12:50:58 +0530

    react navigation added & configured

--- App.js
+++ App.js
@@ -3,17 +3,20 @@ import React from 'react'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
 import CurrentWeather from './src/screens/CurrentWeather'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CurrentWeather/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather />
+      </View>
+    </NavigationContainer>
   )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+  }
 })
